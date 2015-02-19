fb27968db3a4c1fe4569271f7ae395cd21d5a17e
David Tran <dtran320@gmail.com>
2015-02-19 11:13:22 -0800

    Add a simple test for analyze_words

--- tests.py
+++ tests.py
@@ -10,10 +10,17 @@ class TestWriteBetter(unittest.TestCase):
         nltk.download('inaugural')
         self.test_corpus = inaugural.raw('2009-Obama.txt')
 
-    def test_analyze_words(self):
+    def test_analyze_words_obama(self):
         results = analyze_words(self.test_corpus)
         self.assertEquals(results[u'America']['count'], 10)
         self.assertEquals(results[u'America']['pos_tag'], 'NNP')
+
+    def test_analyze_words_simple(self):
+        results = analyze_words("Hi hi hello")
+        self.assertEquals(results[u'hi']['count'], 2)
+        self.assertEquals(results[u'hello']['count'], 1)
+        self.assertEquals(results[u'hi']['pos_tag'], 'NN')
+        self.assertEquals(results[u'hello']['pos_tag'], 'NN')
 
 if __name__ == '__main__':
     unittest.main()

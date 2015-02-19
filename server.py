318de80a0b3ccacffec31b9c10ba3d9387106afb
David Tran <dtran320@gmail.com>
2015-02-19 13:03:16 -0800

    Accept URL as input

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, Markup, render_template, request
+import lxml
+from lxml.html.clean import Cleaner
 
 from app import analyze_words, analyze_sentences, analyze_text
 
@@ -14,10 +16,18 @@ def index():
 @app.route('/analyze', methods=['POST'])
 def analyze():
     text = request.form['text']
+    if not text:
+        url = request.form['url']
+        cleaner = Cleaner()
+        cleaner.javascript = True
+        cleaner.style = True
+        text = Markup(lxml.html.tostring(
+            cleaner.clean_html(lxml.html.parse(url)))).striptags()
+
     result = analyze_words(text)
     sent_results = analyze_sentences(text)
     sent_variance = analyze_text(text)
-    return render_template('result.html',   
+    return render_template('result.html',
         results=result,
         sentence_results=sent_results,
         sent_variance=sent_variance,

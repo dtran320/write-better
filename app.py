be7b3a2a57940bb46ccb0bff034162c1ffd82a92
Tommi Kaikkonen <tommi@kaikkonendesign.fi>
2015-02-19 21:33:25 +0200

    Implementing analyze_sentences, add utility functions

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ import nltk
 from nltk import FreqDist
 from nltk.tokenize import sent_tokenize
 
-def analyze_words(text):
+def get_words(text):
     tokens = nltk.tokenize.word_tokenize(text)
-    pos_tags = nltk.pos_tag(tokens)
-    fdist = FreqDist(tokens)
+    return [word.lower() for word in tokens]
+
+def get_fdist(text):
+    return FreqDist(get_words(text))
+
+def analyze_words(text):
+    words = get_words(text)
+    pos_tags = nltk.pos_tag(words)
+    fdist = FreqDist(words)
 
     result_dict = {}
 
@@ -20,12 +27,29 @@ def analyze_words(text):
 def analyze_sentences(text):
     sent_tokens = sent_tokenize(text)
     result_dict = {}
+
+    fdist = get_fdist(text)
+    n_of_words = fdist.N()
+
+    common_word_threshold = fdist.freq(fdist.most_common(n_of_words // 4)[-1])
+    uncommon_word_threshold = 5 # Needs fixing
+
     for sentence in sent_tokens:
+        common_words = []
+        uncommon_words = []
+        words = get_words(sentence)
+
+        for word in words:
+            if fdist[word] > common_word_threshold:
+                common_words.append(word)
+            if fdist[word] < uncommon_word_threshold:
+                uncommon_words.append(word)
+
         result_dict[sentence] = {
             'length': len(sentence),
-            'common_words': [],
-            'uncommon_words': []
+            'common_words': common_words,
+            'uncommon_words': uncommon_words,
         }
-        
+
     return result_dict
 

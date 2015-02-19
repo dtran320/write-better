293c6bd18b4d1d6cf9fbd6e414cd649b6847bd25
Tommi Kaikkonen <tommi@kaikkonendesign.fi>
2015-02-19 20:46:47 +0200

    returning freq count and pos tag with analyze_words

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import nltk
+from nltk import FreqDist
 
 def analyze_words(text):
     tokens = nltk.tokenize.word_tokenize(text)
     pos_tags = nltk.pos_tag(tokens)
-    return pos_tags
+    fdist = FreqDist(text)
+
+    result_dict = {}
+
+    for word, word_type in pos_tags:
+        result_dict[word] = {
+            'count': fdist[word],
+            'pos_tag': word_type
+        }
+
+    return result_dict
